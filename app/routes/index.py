cbbe3576b8de96ac87adf9fc28ababc4c02fac84
Gabriel Baciu <gabriel.g.baciu@gmail.com>
2014-03-04 21:00:25 -0500

    image resizing via wand

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,7 +6,8 @@ from json import dumps, loads
 from faker import Factory
 import datetime
 import os.path
-
+from wand.image import Image
+from wand.display import display
 
 client = MongoClient('127.0.0.1', 27017)
 db = client.people
@@ -44,14 +45,34 @@ def root():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    """Upload a new file."""
+    """Upload a new file. If file is image then resize one copy and store it in the same folder"""
 
     def secure_filename(f):
         return f
 
+    def isImage(name):
+
+        extensions = ('.jpg', '.JPG', '.jpeg', '.png')
+
+        for x in extensions:
+            if name.endswith(x):
+                return True
+        return False
+    
+    def resize(path, w=0.25, h=0.25):
+        with Image(filename=path) as img:
+            print(img.size)
+            with img.clone() as i:
+                i.resize(int(i.width * w), int(i.height * h))
+                p = path.split("/")
+                i.save(filename="/".join([p[0], p[1], '0.25-' + p[2]]))
+
     def handle_file(f):
         filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        f.save(file_path)
+        if isImage(file_path):
+            resize(file_path)
         return True
 
     if request.method == 'POST':
